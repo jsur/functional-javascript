3e86f234646ceb87d59778b8781e0de68233910b
Julius Suominen <julius.suominen@gmail.com>
2019-10-31 19:56:48 +0200

    add immutability exercise

--- immutability/ex.js
+++ immutability/ex.js
@@ -4,12 +4,22 @@ function lotteryNum() {
 	return (Math.round(Math.random() * 100) % 58) + 1;
 }
 
-function pickNumber() {}
+function pickNumber(num, numbers = []) {
+	const newNumbers = [ ...numbers ]
+	if (!newNumbers.includes(num)) {
+		newNumbers.push(num)
+		newNumbers.sort((a, b) => a - b)
+	}
+	return newNumbers
+}
 
 var luckyLotteryNumbers = [];
 
 while (luckyLotteryNumbers.length < 6) {
-	pickNumber();
+	luckyLotteryNumbers = pickNumber(
+		lotteryNum(),
+		Object.freeze(luckyLotteryNumbers)
+	);
 }
 
 console.log(luckyLotteryNumbers);
